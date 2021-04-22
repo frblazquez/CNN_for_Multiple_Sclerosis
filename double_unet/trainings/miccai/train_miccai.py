cd3b05b8404fcab1da53f38d104444631bf21ae4
Francisco Javier Blázquez Martínez <frblazquezm@gmail.com>
2021-04-22 12:03:21 +0200

    Double U-Net MICCAI training with profiling

--- double_unet/trainings/miccai/train_miccai.py
+++ double_unet/trainings/miccai/train_miccai.py
@@ -6,6 +6,7 @@
 # Description:
 # Double U-Net train over MICCAI 2016 Multiple Sclerosis lessions dataset.
 
+import os
 import argparse
 import sys
 
@@ -13,12 +14,12 @@ import pyeddl.eddl as eddl
 from pyeddl.tensor import Tensor
 from double_unet import double_unet
 
-
 MICCAI_PATH  = "/scrap/users/blazquez/datasets/miccai2016/"
 MEM_CHOICES = ("low_mem", "mid_mem", "full_mem")
 LOSS_FUNCTION = "binary_cross_entropy"
 METRICS = "dice"
 LEARNING_RATE = 0.00001
+PROFILE_CMD = 'nvidia-smi --query-gpu=power.draw,utilization.gpu,utilization.memory,temperature.gpu,pstate, --format=csv,nounits --id=0 -lms 500 -f ./GPUprofile' 
 
 
 def main(args):
@@ -58,19 +59,21 @@ def main(args):
     for i in range(args.epochs):
         print("Epoch %d/%d" % (i + 1, args.epochs))
         eddl.reset_loss(net)
+        os.system(PROFILE_CMD + str(i) + '.csv &')
         for j in range(args.num_batches):
             eddl.next_batch([x_train, y_train], [x_batch, y_batch])
             eddl.train_batch(net, [x_batch], [y_batch])
             eddl.print_loss(net, j)
             print()
+        os.system('kill $(ps aux | grep \'nvidia-smi\' | awk \'{print $2}\')')
         print()
-        eddl.save("models/double_unet_miccai_"+str(i)+".bin")
+        eddl.save(net, "models/double_unet_miccai_"+str(i)+".bin")
         eddl.evaluate(net, [x_test], [y_test])  
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("--epochs", type=int, metavar="INT", default=64)
+    parser.add_argument("--epochs", type=int, metavar="INT", default=128)
     parser.add_argument("--batch-size", type=int, metavar="INT", default=8)
     parser.add_argument("--num-batches", type=int, metavar="INT", default=1152)
     parser.add_argument("--mem", metavar="|".join(MEM_CHOICES), choices=MEM_CHOICES, default="low_mem")
